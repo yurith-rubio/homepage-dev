bc82f7d9475b70d38b342a49ff6aeab8469393a7
Yurith Rubio <iamyurith@gmail.com>
2023-08-02 08:39:36 +0200

    nav changes

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar(){
             <div className='nav-bar-content content-boundary'>
                 <NavButton link='/services' children='Services' className='active'/>
                 <NavButton link='/projects' children='Projects'/>
-                <NavButton link='/yurith-says' children='Blog'/>
+                <NavButton link='/yurith-says/' children='Blog'/>
                 <NavButton link='/about' children='About'/>
                 <NavButton link='/contact' children='Contact'/>
             </div>
